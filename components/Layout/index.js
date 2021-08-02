c01e3e0f67238ce6032a1e41a5f3e586cdbe97c3
Michael Arakilian <arakilian0@gmail.com>
2021-08-02 14:06:29 -0700

    Create Layout Component

---
+++ components/Layout/index.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import Header from './Header'
+
+function Layout(props) {
+    return(
+        <Fragment>
+            <Header />
+            { props.children }
+        </Fragment>
+    )
+}
+
+export default Layout
